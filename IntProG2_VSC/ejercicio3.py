c8180cca91239ade27a1e8099460ad2694da39c2
Zahid Reyes <164809503+ZazaJack@users.noreply.github.com>
2025-04-01 11:01:26 -0600

    Delete IntProG2_VSC directory

--- IntProG2_VSC/ejercicio3.py
+++
@@ -1,6 +0,0 @@
-nombre= input("Ingrese su nombre: ")
-edad= int(input("Ingrese su edad: "))
-peso= float(input("Ingrese su peso: "))
-edad_dias = edad * 365
-peso_kg = peso / 2.204
-print("Hola", nombre, "tu edad en días es", edad_dias, "tu peso en kg es", peso_kg)
